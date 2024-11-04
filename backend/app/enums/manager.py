638480eac7cc1cf48d65e1a0652406f7eeb38b0b
Jhon <jhon.rodriguez22329@ucaldas.edu.co>
2024-11-03 21:36:47 -0500

    db tables refactor and debts table

--- backend/app/enums/manager.py
+++ backend/app/enums/manager.py
@@ -1,5 +1,6 @@
 class CSVModels:
     PRODUCTOS: str = 'productos.csv'
     VENTAS: str = 'ventas.csv'
+    DEUDAS: str = 'deudas.csv'
     DEUDORES: str = 'deudores.csv'
     VENTA_PRODUCTOS = 'venta_productos.csv' 
