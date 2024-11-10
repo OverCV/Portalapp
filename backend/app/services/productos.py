8d1e4bdb55c8e9a5e45428e5ee7f876b721b7409
maximuscack <juancarloslopezvalencia35@gmail.com>
2024-11-09 20:51:27 -0500

    productos

--- backend/app/services/productos.py
+++ backend/app/services/productos.py
@@ -1,7 +1,12 @@
-from backend.data.manager import Manager
+from backend.data.managers.manager import Manager
+from backend.models.producto import Producto
 
 
-def put_producto(data_manager: Manager, id_producto: dict, nuevo_producto: dict):
+def put_producto(
+    data_manager: Manager,
+    id_producto: Producto,
+    nuevo_producto: Producto,
+) -> None:
     return data_manager.put_data(
         'productos',
         id_producto,
